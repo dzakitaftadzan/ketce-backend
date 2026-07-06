91b78941805668ac387b5245360245c7339e165c
dzakitaftadzan <dzakitaftad@gmail.com>
2026-07-06 18:23:10 +0800

    feat: penambahan model, migration, dan seeder untuk kategori

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $categories = ['Pakaian Pria', 'Pakaian Wanita', 'Sepatu', 'Aksesoris'];
+        foreach ($categories as $cat) {
+            Category::updateOrCreate(
+                ['slug' => Str::slug($cat)],
+                ['name' => $cat]
+            );
+        }
+    }
+}
